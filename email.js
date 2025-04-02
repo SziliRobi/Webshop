6b4d0954f91b7d9382bda77ab094725bcaf07630
SziliRobi <szili.istvan.robert@gmail.com>
2025-04-02 22:24:39 +0200

    a

---
+++ email.js
@@ -0,0 +1,12 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_7jtkw38","template_qjj8n3p",parms).then(alert("Email Sent!"))
+
+
+}
